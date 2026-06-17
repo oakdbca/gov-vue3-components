866d4c12c21c4d79148c28800d493ea90763fcef
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2026-06-17 16:24:27 +0800

    Refactor component exports in index.js so clients can import individual components rather than having to import all components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import components from'./components'
+export { default as BootstrapAlert } from './components/BootstrapAlert.vue'
+export { default as BootstrapButtonSpinner } from './components/BootstrapButtonSpinner.vue'
+export { default as BootstrapModal } from './components/BootstrapModal.vue'
+export { default as BootstrapSpinner } from './components/BootstrapSpinner.vue'
+export { default as CollapsibleComponent } from './components/CollapsibleComponent.vue'
+export { default as ErrorRenderer } from './components/ErrorRenderer.vue'
+export { default as FormSection } from './components/FormSection.vue'
+export { default as PanelAssignable } from './components/PanelAssignable.vue'
+export { default as PanelStatus } from './components/PanelStatus.vue'
+export { default as PanelWorkflow } from './components/PanelWorkflow.vue'
+export { default as Select2Search } from './components/Select2Search.vue'
+export { default as SelectAssignable } from './components/SelectAssignable.vue'
+export { default as StatusPanel } from './components/StatusPanel.vue'
+
+import components from './components'
 
 const plugin = {
-  install (Vue) {
-    for (const prop in components) {
-      if (components.hasOwnProperty(prop)) {
-        const component = components[prop]
-        Vue.component(component.name, component)
-      }
-    }
-  }
+    install(Vue) {
+        for (const component of Object.values(components)) {
+            Vue.component(component.name, component)
+        }
+    },
 }
 
 export default plugin
